ccdaca56469d145331627c157f5a0a1ede600c51
wrathchild14 <jovanprodanov123@gmail.com>
2023-05-20 19:57:09 +0200

    refactor code, add calculation of std dev, add storing of data

--- detection/detection.ino
+++ detection/detection.ino
@@ -7,35 +7,38 @@
 #define REG_GYRO_XOUT_H 0x43
 
 // calibration values
-float accelOffsetX = 0.0f;
-float accelOffsetY = 0.0f;
-float accelOffsetZ = 0.0f;
-float gyroOffsetX = 0.0f;
-float gyroOffsetY = 0.0f;
-float gyroOffsetZ = 0.0f;
+float accel_off_x = 0.0f;
+float accel_off_y = 0.0f;
+float accel_off_z = 0.0f;
+float gyro_off_x = 0.0f;
+float gyro_off_y = 0.0f;
+float gyro_off_z = 0.0f;
 
-const float ACCELERATION_THRESHOLD = 0.8f;
-const float PEAK_MINIMUM_THRESHOLD = 0.5f;
-const float JUMP_THRESHOLD = 1.5f;
+const float WALKING_THRESHOLD = 1.f;
+const float RUNNING_THRESHOLD = 4.f;
 
 const int TICKER_INTERVAL = 100;
 
 Ticker ticker;
 
-float previousAccelValue = 0.0f;
-float currentAccelValue = 0.0f;
-bool isStep = false;
-bool isJump = false;
+// classification properties
+const int DATA_SIZE = 10;
+float roll_data[DATA_SIZE];
+float pitch_data[DATA_SIZE];
+float vertical_data[DATA_SIZE];
+float horizontal_data[DATA_SIZE];
+int data_idx = 0;
+float get_std_dev(const float* data, int data_size);
 
-void calibrateSensors() {
+void calibrate() {
   int iterations = 500;
 
-  float accelX = 0.0f;
-  float accelY = 0.0f;
-  float accelZ = 0.0f;
-  float gyroX = 0.0f;
-  float gyroY = 0.0f;
-  float gyroZ = 0.0f;
+  float acc_x = 0.0f;
+  float acc_y = 0.0f;
+  float acc_z = 0.0f;
+  float gyro_x = 0.0f;
+  float gyro_y = 0.0f;
+  float gyro_z = 0.0f;
 
   for (int i = 0; i < iterations; i++) {
     Wire.beginTransmission(MAIN_REG_ADDR);
@@ -43,9 +46,9 @@ void calibrateSensors() {
     Wire.endTransmission();
     Wire.requestFrom(MAIN_REG_ADDR, 6);
 
-    int16_t accelRawX = (Wire.read() << 8) | Wire.read();
-    int16_t accelRawY = (Wire.read() << 8) | Wire.read();
-    int16_t accelRawZ = (Wire.read() << 8) | Wire.read();
+    int16_t acc_raw_x = (Wire.read() << 8) | Wire.read();
+    int16_t acc_ray_y = (Wire.read() << 8) | Wire.read();
+    int16_t acc_ray_z = (Wire.read() << 8) | Wire.read();
 
     Wire.beginTransmission(MAIN_REG_ADDR);
     Wire.write(REG_GYRO_XOUT_H);
@@ -53,55 +56,55 @@ void calibrateSensors() {
 
     Wire.requestFrom(MAIN_REG_ADDR, 6);
 
-    int16_t gyroRawX = (Wire.read() << 8) | Wire.read();
-    int16_t gyroRawY = (Wire.read() << 8) | Wire.read();
-    int16_t gyroRawZ = (Wire.read() << 8) | Wire.read();
+    int16_t gyro_raw_x = (Wire.read() << 8) | Wire.read();
+    int16_t gyro_raw_y = (Wire.read() << 8) | Wire.read();
+    int16_t gyro_raw_z = (Wire.read() << 8) | Wire.read();
 
-    accelX += accelRawX / 16384.0f;
-    accelY += accelRawY / 16384.0f;
-    accelZ += accelRawZ / 16384.0f;
-    gyroX += gyroRawX / 131.0f;
-    gyroY += gyroRawY / 131.0f;
-    gyroZ += gyroRawZ / 131.0f;
+    acc_x += acc_raw_x / 16384.0f;
+    acc_y += acc_ray_y / 16384.0f;
+    acc_z += acc_ray_z / 16384.0f;
+    gyro_x += gyro_raw_x / 131.0f;
+    gyro_y += gyro_raw_y / 131.0f;
+    gyro_z += gyro_raw_z / 131.0f;
 
     delay(2);
   }
 
   // [-1, 1]
-  accelOffsetX = accelX / iterations;
-  accelOffsetY = accelY / iterations;
+  accel_off_x = acc_x / iterations;
+  accel_off_y = acc_y / iterations;
   // [0, 2]
-  accelOffsetZ = (accelZ / iterations) - 1.0f;  // 1g for gravity
+  accel_off_z = (acc_z / iterations) - 1.0f;  // 1g for gravity
 
-  gyroOffsetX = gyroX / iterations;
-  gyroOffsetY = gyroY / iterations;
-  gyroOffsetZ = gyroZ / iterations;
+  gyro_off_x = gyro_x / iterations;
+  gyro_off_y = gyro_y / iterations;
+  gyro_off_z = gyro_z / iterations;
 
-  Serial.println("Sensor calibration complete.");
-  Serial.print("Accelerometer offsets (X,Y,Z): ");
-  Serial.print(accelOffsetX);
+  Serial.println("calibration complete");
+  Serial.print("acc offsets (X, Y, Z): ");
+  Serial.print(accel_off_x);
   Serial.print(", ");
-  Serial.print(accelOffsetY);
+  Serial.print(accel_off_y);
   Serial.print(", ");
-  Serial.println(accelOffsetZ);
-  Serial.print("Gyroscope offsets (X,Y,Z): ");
-  Serial.print(gyroOffsetX);
+  Serial.println(accel_off_z);
+  Serial.print("gyroscope offsets (X, Y, Z): ");
+  Serial.print(gyro_off_x);
   Serial.print(", ");
-  Serial.print(gyroOffsetY);
+  Serial.print(gyro_off_y);
   Serial.print(", ");
-  Serial.println(gyroOffsetZ);
+  Serial.println(gyro_off_z);
 }
 
-void detectWalking() {
+void activity_detect() {
   Wire.beginTransmission(MAIN_REG_ADDR);
   Wire.write(REG_ACCEL_XOUT_H);
   Wire.endTransmission();
 
   Wire.requestFrom(MAIN_REG_ADDR, 6);
 
-  int16_t accelRawX = (Wire.read() << 8) | Wire.read();
-  int16_t accelRawY = (Wire.read() << 8) | Wire.read();
-  int16_t accelRawZ = (Wire.read() << 8) | Wire.read();
+  int16_t acc_raw_x = (Wire.read() << 8) | Wire.read();
+  int16_t acc_ray_y = (Wire.read() << 8) | Wire.read();
+  int16_t acc_ray_z = (Wire.read() << 8) | Wire.read();
 
   Wire.beginTransmission(MAIN_REG_ADDR);
   Wire.write(REG_GYRO_XOUT_H);
@@ -109,54 +112,97 @@ void detectWalking() {
 
   Wire.requestFrom(MAIN_REG_ADDR, 6);
 
-  int16_t gyroRawX = (Wire.read() << 8) | Wire.read();
-  int16_t gyroRawY = (Wire.read() << 8) | Wire.read();
-  int16_t gyroRawZ = (Wire.read() << 8) | Wire.read();
+  int16_t gyro_raw_x = (Wire.read() << 8) | Wire.read();
+  int16_t gyro_raw_y = (Wire.read() << 8) | Wire.read();
+  int16_t gyro_raw_z = (Wire.read() << 8) | Wire.read();
 
   // 2^14
-  float accelX = (accelRawX / 16384.0f) - accelOffsetX;
-  float accelY = (accelRawY / 16384.0f) - accelOffsetY;
-  float accelZ = (accelRawZ / 16384.0f) - accelOffsetZ;
-  float gyroX = (gyroRawX / 131.0f) - gyroOffsetX;
-  float gyroY = (gyroRawY / 131.0f) - gyroOffsetY;
-  float gyroZ = (gyroRawZ / 131.0f) - gyroOffsetZ;
+  float acc_x = (acc_raw_x / 16384.0f) - accel_off_x;
+  float acc_y = (acc_ray_y / 16384.0f) - accel_off_y;
+  float acc_z = (acc_ray_z / 16384.0f) - accel_off_z;
+  float gyro_x = (gyro_raw_x / 131.0f) - gyro_off_x;
+  float gyro_y = (gyro_raw_y / 131.0f) - gyro_off_y;
+  float gyro_z = (gyro_raw_z / 131.0f) - gyro_off_z;
 
-  Serial.print(accelX);
-  Serial.print(" ");
-  Serial.print(accelY);
-  Serial.print(" ");
-  Serial.print(accelZ);
+  // pritning of accel and gyro data
+  // Serial.print(acc_x);
+  // Serial.print(" ");
+  // Serial.print(acc_y);
+  // Serial.print(" ");
+  // Serial.print(acc_z);
+  // Serial.println();
+  // Serial.print(gyro_x);
+  // Serial.print(" ");
+  // Serial.print(gyro_y);
+  // Serial.print(" ");
+  // Serial.print(gyro_z);
+  // Serial.println();
 
-  Serial.println();
+  float acceleration = sqrt(acc_x * acc_x + acc_y * acc_y + acc_z * acc_z);
 
-  Serial.print(gyroX);
-  Serial.print(" ");
-  Serial.print(gyroY);
-  Serial.print(" ");
-  Serial.print(gyroZ);
+  float roll = atan2(acc_y, acc_z) * (180.0 / PI);
+  float pitch = atan2(-acc_x, sqrt(acc_y * acc_y + acc_z * acc_z)) * (180.0 / PI);
 
-  Serial.println();
+  float vert_comp_acc = acc_z * cos(pitch * (PI / 180.0));
+  float hor_comp_acc = acc_z * sin(pitch * (PI / 180.0));
 
-  float acceleration = sqrt(accelX * accelX + accelY * accelY + accelZ * accelZ);
+  // Serial.print("acceleration: ");
+  // Serial.print(acceleration);
+  // Serial.print("   roll: ");
+  // Serial.print(roll);
+  // Serial.print("   pitch: ");
+  // Serial.print(pitch);
+  // Serial.println();
 
-  float roll = atan2(accelY, accelZ) * (180.0 / PI);
-  float pitch = atan2(-accelX, sqrt(accelY * accelY + accelZ * accelZ)) * (180.0 / PI);
+  roll_data[data_idx] = roll;
+  pitch_data[data_idx] = pitch;
+  vertical_data[data_idx] = vert_comp_acc;
+  horizontal_data[data_idx] = hor_comp_acc;
+  data_idx = (data_idx + 1) % DATA_SIZE;
 
-  Serial.print("acceleration: ");
-  Serial.print(acceleration);
-  Serial.print("   roll: ");
-  Serial.print(roll);
-  Serial.print("   pitch: ");
-  Serial.print(pitch);
+  float roll_std_dev = get_std_dev(roll_data, DATA_SIZE);
+  float pitch_std_dev = get_std_dev(pitch_data, DATA_SIZE);
+  float vert_std_dev = get_std_dev(vertical_data, DATA_SIZE);
+  float hor_std_dev = get_std_dev(horizontal_data, DATA_SIZE);
+
+  Serial.print("roll_std_dev: ");
+  Serial.print(roll_std_dev);
+  Serial.print(" pitch_std_dev: ");
+  Serial.print(pitch_std_dev);
+  Serial.print(" vert_std_dev: ");
+  Serial.print(vert_std_dev);
+  Serial.print(" hor_std_dev: ");
+  Serial.print(hor_std_dev);
   Serial.println();
 }
 
 void setup() {
   Serial.begin(115200);
   Wire.begin(12, 14);
-  calibrateSensors();
-  ticker.attach_ms(TICKER_INTERVAL, detectWalking);
+  calibrate();
+  ticker.attach_ms(TICKER_INTERVAL, activity_detect);
 }
 
 void loop() {
+}
+
+float get_std_dev(const float* data, int data_size) {
+  float sum = 0.0f;
+  float mean = 0.0f;
+
+  for (int i = 0; i < data_size; i++) {
+    sum += data[i];
+  }
+  mean = sum / data_size;
+
+  float squared_diff_sum = 0.0f;
+  for (int i = 0; i < data_size; i++) {
+    float diff = data[i] - mean;
+    squared_diff_sum += (diff * diff);
+  }
+
+  float variance = squared_diff_sum / data_size;
+  float std_dev = sqrt(variance);
+
+  return std_dev;
 }
