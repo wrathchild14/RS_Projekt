a3267ad6848276884615ff85a25edf0432ebaa2c
wrathchild14 <jovanprodanov123@gmail.com>
2023-05-21 13:28:21 +0200

    add css code

--- detection/server.cpp
+++ detection/server.cpp
@@ -1,7 +1,23 @@
 #include "server.h"
 
 void handleRoot() {
-  String html = "<html><head><script>"
+  String html = "<html><head>"
+                "<style>"
+                "body {"
+                "  font-family: Arial, sans-serif;"
+                "  background-color: #f2f2f2;"
+                "}"
+                "h1 {"
+                "  color: #333333;"
+                "  text-align: center;"
+                "}"
+                "#activity {"
+                "  font-size: 24px;"
+                "  font-weight: bold;"
+                "  display: block;"
+                "}"
+                "</style>"
+                "<script>"
                 "function getActivity() {"
                 "  var xhttp = new XMLHttpRequest();"
                 "  xhttp.onreadystatechange = function() {"
@@ -12,7 +28,7 @@ void handleRoot() {
                 "  xhttp.open('GET', '/activity', true);"
                 "  xhttp.send();"
                 "}"
-                "setInterval(getActivity, 1000);" // get activity every 1 second
+                "setInterval(getActivity, 1000);"  // get activity every 1 second
                 "</script></head><body>"
                 "<h1>Activity: <span id=\"activity\"></span></h1>"
                 "</body></html>";
